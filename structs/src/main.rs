fcca265163dc6873bbcd64c0aec1c77fc18c90a0
bellatoris <bellatoris@snu.ac.kr>
2021-08-25 16:41:47 +0900

    update

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -11,7 +11,6 @@ fn main() {
     println!("user.email = {}", user1.email);
 
     let user1 = build_user("nahco3_hanmail.net".to_string(), "hahasiyoon".to_string());
-    
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
@@ -32,10 +31,7 @@ fn build_user(email: String, username: String) -> User {
 fn print_user(user: User) -> String {
     format!(
         "user's name is {}, email is {}, active is {} and sign in count is {}",
-        user.email,
-        user.username,
-        user.active,
-        user.sign_in_count
+        user.email, user.username, user.active, user.sign_in_count
     )
 }
 
